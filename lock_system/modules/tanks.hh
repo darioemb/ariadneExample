0a7778d41017daeb1c1870e8e435290416c71d27
sphero <sphero@outlook.it>
2018-04-20 08:49:08 +0200

    Lock system correct. To test because too heavy

---
+++ lock_system/modules/tanks.hh
@@ -0,0 +1,118 @@
+#pragma once
+#include <ariadne.h>
+
+namespace LockSystem
+{
+HybridIOAutomaton getSystem(
+    RealVariable a1,
+    RealVariable a2,
+    RealVariable z1,
+    RealVariable z2,
+    RealParameter alpha1,
+    RealParameter alpha2,
+    RealParameter beta1,
+    RealParameter beta2,
+    RealParameter baseLevel1,
+    RealParameter baseLevel2,    
+    DiscreteLocation S0)
+{
+    // 1.Automaton registration
+    HybridIOAutomaton lock_system("lock_system");
+
+    // 2.Registration of input/output variables
+    lock_system.add_input_var(a1);
+    lock_system.add_input_var(a2);
+    lock_system.add_output_var(z1);
+    lock_system.add_output_var(z2);
+    
+    
+    DiscreteEvent e_a("e_a");
+    lock_system.add_output_event(e_a);
+    DiscreteEvent e_b("e_b");
+    lock_system.add_output_event(e_b);
+    DiscreteEvent e_c("e_c");
+    lock_system.add_output_event(e_c);
+    DiscreteEvent e_d("e_d");
+    lock_system.add_output_event(e_d);
+    
+  
+
+    // 4.Registration of Locations
+    DiscreteLocation S1("S1");
+    DiscreteLocation S2("S2");
+    DiscreteLocation S3("S3");
+    
+
+    lock_system.new_mode(S0); // empty both tank
+    lock_system.new_mode(S1); // tank1 hmax=~4m
+    lock_system.new_mode(S2); // tank2 hmax=~7m
+    lock_system.new_mode(S3); // both not empty
+
+    // 5.Registration of dynamics
+    
+    
+    //empty initial tank
+    lock_system.set_dynamics(S0, z1, beta1 * a1);
+    lock_system.set_dynamics(S0, z2, beta2 * a2);
+    
+    //tank1 not empty
+    lock_system.set_dynamics(S1, z1, -alpha1 * z1 + beta1 * a1 );
+    lock_system.set_dynamics(S1, z2, beta2 * a2);
+    
+    
+    //empty 2 not empty  tank1 empty
+    lock_system.set_dynamics(S2, z1, beta1 * a1);
+    lock_system.set_dynamics(S2, z2, -alpha2 * z2 + beta2 * a2);
+
+    //both tank with water
+    lock_system.set_dynamics(S3, z1, -alpha1 * z1 + beta1 * a1 );
+    lock_system.set_dynamics(S3, z2, -alpha2 * z2 + beta2 * a2);
+    
+
+    //guards
+    
+    RealExpression guard_a = z1 - baseLevel1;      
+    
+    RealExpression guard_b = z2 - baseLevel2;
+
+    RealExpression guard_c = -z1 + baseLevel1;
+
+    RealExpression guard_d = -z2 + baseLevel2;
+
+
+    
+
+    std::map<RealVariable, RealExpression> reset;
+    reset[z1] = z1;
+    reset[z2] = z2;
+    
+    // // reset[time] = time;
+
+    /**
+         * 			TRANSITION TABLE
+         * --------------------
+         * 	 |	S0	S1	S2  S3		
+         * --------------------
+         * a |	S1	-	S3	-	
+         * b |	S2	S3	-	-	
+         * c |  -   S0  -   S2
+         * d |  -   -   S0  S1
+         */
+    lock_system.new_forced_transition(e_a, S0, S1,  reset, guard_a);
+    lock_system.new_forced_transition(e_b, S0, S2, reset, guard_b);
+
+    lock_system.new_forced_transition(e_c, S1, S0, reset, guard_c);
+    lock_system.new_forced_transition(e_b, S1, S3, reset, guard_b);
+
+    lock_system.new_forced_transition(e_a, S2, S3, reset, guard_a);
+    lock_system.new_forced_transition(e_d, S2, S0, reset, guard_d);
+
+    lock_system.new_forced_transition(e_c, S3, S2, reset, guard_c);
+    lock_system.new_forced_transition(e_d, S3, S1, reset, guard_d);
+
+
+
+    
+    return lock_system;
+}
+}
---
+++ lock_system/modules/valves.hh
@@ -0,0 +1,114 @@
+#pragma once
+#include <ariadne.h>
+
+namespace oneValve
+{
+HybridIOAutomaton getSystem(
+    RealVariable a1, 
+    RealParameter T, 
+    DiscreteEvent e_a1_open,
+    DiscreteEvent e_a1_close, 
+    DiscreteLocation a1_idle)
+{
+    // 1.Automaton registration
+    HybridIOAutomaton valve("valve1");
+
+    // 2.Registration of input/output
+    valve.add_output_var(a1);
+
+    // 3.Registration of input/output internal events
+     DiscreteEvent e_a1_idle("a1_idle");
+
+    valve.add_input_event(e_a1_open);
+    valve.add_input_event(e_a1_close);
+    valve.add_internal_event(e_a1_idle);
+
+    // 4.Registration of locations
+     DiscreteLocation a1_opening("a1_opening");
+     DiscreteLocation a1_closing("a1_closing");
+
+    valve.new_mode(a1_opening);
+    valve.new_mode(a1_idle);
+    valve.new_mode(a1_closing);
+
+    // 5.Registration of dynamics
+    valve.set_dynamics(a1_idle, a1, 0.0);
+    valve.set_dynamics(a1_closing, a1, -1.0 / T);
+    valve.set_dynamics(a1_opening, a1, 1.0 / T);
+
+    // 6.Registration of transitions
+    // Guards
+    RealExpression a1_geq_one = a1 - 1.0; // a >= 1
+    RealExpression a1_leq_zero = -a1;     // a >= 0
+
+    // Resets
+    std::map<RealVariable, RealExpression> rst_a1_one;
+    rst_a1_one[a1] = 1.0; // a = 1
+    std::map<RealVariable, RealExpression> rst_a1_zero;
+    rst_a1_zero[a1] = 0.0; // a = 0
+
+    valve.new_forced_transition(e_a1_idle, a1_opening, a1_idle, rst_a1_one, a1_geq_one);
+    valve.new_forced_transition(e_a1_idle, a1_closing, a1_idle, rst_a1_zero, a1_leq_zero);
+
+    valve.new_unforced_transition(e_a1_open, a1_idle, a1_opening);
+    valve.new_unforced_transition(e_a1_close, a1_idle, a1_closing);
+
+    return valve;
+}
+}
+namespace twoValve
+{
+HybridIOAutomaton getSystem(
+    RealVariable a2, 
+    RealParameter T, 
+    DiscreteEvent e_a2_open,
+    DiscreteEvent e_a2_close, 
+    DiscreteLocation a2_idle)
+{
+    // 1.Automaton registration
+    HybridIOAutomaton valve("valve2");
+
+    // 2.Registration of input/output
+    valve.add_output_var(a2);
+
+    // 3.Registration of input/output internal events
+    DiscreteEvent e_a2_idle("a2_idle");
+
+    valve.add_input_event(e_a2_open);
+    valve.add_input_event(e_a2_close);
+    valve.add_internal_event(e_a2_idle);
+
+    // 4.Registration of locations
+    DiscreteLocation a2_opening("a2_opening");
+    DiscreteLocation a2_closing("a2_closing");
+    //DiscreteLocation a2_idle("a2_idle");
+
+    valve.new_mode(a2_opening);
+    valve.new_mode(a2_idle);
+    valve.new_mode(a2_closing);
+
+    // 5.Registration of dynamics
+    valve.set_dynamics(a2_idle, a2, 0.0);
+    valve.set_dynamics(a2_closing, a2, -1.0 / T);
+    valve.set_dynamics(a2_opening, a2, 1.0 / T);
+
+    // 6.Registration of transitions
+    // Guards
+    RealExpression a2_geq_one = a2 - 1.0; // a >= 1
+    RealExpression a2_leq_zero = -a2;     // a >= 0
+
+    // Resets
+    std::map<RealVariable, RealExpression> rst_a2_one;
+    rst_a2_one[a2] = 1.0; // a = 1
+    std::map<RealVariable, RealExpression> rst_a2_zero;
+    rst_a2_zero[a2] = 0.0; // a = 0
+
+    valve.new_forced_transition(e_a2_idle, a2_opening, a2_idle, rst_a2_one, a2_geq_one);
+    valve.new_forced_transition(e_a2_idle, a2_closing, a2_idle, rst_a2_zero, a2_leq_zero);
+
+    valve.new_unforced_transition(e_a2_open, a2_idle, a2_opening);
+    valve.new_unforced_transition(e_a2_close, a2_idle, a2_closing);
+
+    return valve;
+}
+}
